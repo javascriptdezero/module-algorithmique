5d3a73683889a8254d360cf06da66e6a7a978d1b
JavaScript de Zéro <jeremy@javascriptdezero.com>
2022-06-09 17:27:14 +0200

    Ajout du cours sur les permutations

---
+++ cours/16 - Permutations/cours.js
@@ -0,0 +1,14 @@
+/* Les permutations */
+
+/* Générer les permutations "à la main" */
+
+// Permutations de l'ensemble E = [1,2] noté (12)
+
+// (1,2,3)
+
+// (1,2,3,4)
+
+// Formule du dénombrement
+
+// Voir https://excalidraw.com/#json=cusHm33AAUv2Z4HZ5_eYG,vh5Lz-WZd35Zq8sAyN9dBg
+
