a3a9a622d91120df77421d5af6cff2760b9a33c3
JavaScript de Zéro <jeremy@javascriptdezero.com>
2022-06-13 12:00:54 +0200

    Leçon sur les arrangements

---
+++ cours/17 - Arrangements/cours.js
@@ -0,0 +1,11 @@
+/* Les arrangements (sans répétitions) */
+
+/* Générer les arrangements "à la main" */
+
+// Arrangements de k = 2 éléments dans E = [1,2,3] (n = 3)
+
+// Arrangements de k = 3 éléments dans E = [1,2,3,4] (n = 4)
+
+// Formule du dénombrement
+
+// Voir https://excalidraw.com/#json=uSx4ttjQSWfjDYC7aiWJD,Sz_2t-_7qvlLc09qZC3Jfg
